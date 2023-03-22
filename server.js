9aa5072a3e8308d6dfa524f4337d731b34cd5023
Jhony14 <jhonyart14@gmail.com>
2023-03-22 14:15:31 +0100

    - Add initial bot information

--- server.js
+++ server.js
@@ -35,11 +35,12 @@ app.post('/chat', async (req, res) => {
 
   // Preparamos el prompt incluyendo el historial de la conversación
   const prompt = conversationHistory.join('\n') + '\nKitty:';
+  const initialInfo = 'Te llamas kitty, Y estas hablando con un humano, tu creador es Jhonatan y tu Vives en Valencia, España, ajusta tu fecha y hora a GMT+1 concretamente '+ new Date() +', intenta se amigable y seguir la conversación con los últimos mensajes.';
 
   try {
     const response = await openai.createCompletion({
       model: 'text-davinci-003',
-      prompt: prompt,
+      prompt: initialInfo + prompt,
       max_tokens: 250,
       n: 1,
       stop: null,
