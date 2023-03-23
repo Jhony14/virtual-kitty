f37e6494b089c40e5071245aa9575dd4b34a207f
Jhony14 <jhonyart14@gmail.com>
2023-03-23 23:50:09 +0100

    - Add log system

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv');
 const { Configuration, OpenAIApi } = require("openai");
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 
 dotenv.config();
 
@@ -17,13 +19,11 @@ app.use(express.static('public'));
 
 
 const corsOptions = {
-  origin: 'https://kitty.dev-ja.cyou/',
+  origin: process.env.URL,
   optionsSuccessStatus: 200,
 };
-
 app.use(cors(corsOptions));
 
-//app.use(cors());
 app.use(bodyParser.json());
 
 app.post('/chat', async (req, res) => {
@@ -34,7 +34,7 @@ app.post('/chat', async (req, res) => {
   }
 
   const prompt = conversationHistory.join('\n') + '\nKitty:';
-  const initialInfo = 'Te llamas kitty, Y estas hablando con un humano, tu creador es Jhonatan y tu Vives en Valencia, España, ajusta tu fecha y hora a GMT+1 concretamente '+ new Date() +', intenta se amigable y seguir la conversación con los últimos mensajes.';
+  const initialInfo = 'Te llamas kitty, Y estas hablando con un humano, tu creador es Jhonatan y tu Vives en Valencia, España, ajusta tu fecha y hora a GMT+1 concretamente '+ getDate() +', intenta se amigable y seguir la conversación con los últimos mensajes, si en los primeros mensajes el nombre del humano es Usuario preguntar si quiere que le llames de otra forma y que puede añadir su nombre arriba.';
 
   try {
     const response = await openai.createCompletion({
@@ -47,13 +47,37 @@ app.post('/chat', async (req, res) => {
     });
 
     const chatGPTResponse = response.data.choices[0].text.trim();
-
+    saveLogs(prompt);
     res.json({ response: chatGPTResponse });
   } catch (error) {
-    console.error(error);
+    console.error('Error al procesar tu mensaje:', error);
     res.status(500).json({ error: 'Error al procesar tu mensaje.' });
   }
 });
+
+function getDate() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  return `${year}-${month}-${day}`;
+}
+
+function saveLogs(data, isError = false) {
+  const messageType = isError ? 'stderr' : 'stdout';
+  const fileName = `${messageType}_${getDate()}.log`;
+  const logsFolderPath = 'logs';
+  if (!fs.existsSync(logsFolderPath)) {
+    fs.mkdirSync(logsFolderPath);
+  }
+  const filePath = path.join(logsFolderPath, fileName);
+
+  fs.writeFile(filePath, data, (err) => {
+    if (err) {
+      console.error('Error al guardar el archivo:', err);
+    }
+  });
+}
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor iniciado en el puerto ${PORT}`));
